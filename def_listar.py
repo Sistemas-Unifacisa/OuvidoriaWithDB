b6a298778e4ab142b2974022eb40016c15fc83dc
lucas.sarruda <lucassol133@gmail.com>
2025-04-01 23:53:16 -0300

    adicionado algumas mudanças no def de listar e comentarios

--- def_listar.py
+++ def_listar.py
@@ -1,12 +1,23 @@
 from operacoesbd import listarBancoDados
 
-
 def listar_manifestações(conn):
     sql = (
         "SELECT * FROM manifestacao"  # definir em uma variavel o que eu quero passar pro SQL
-    )
-
-    listar_manifestações = listarBancoDados(conn, sql)
+)
+    manifestações = listarBancoDados(conn, sql)
     # com os paranmentros definidos, enviamos pro BANCO DE DADOS e ele vai ler o (sql) que eu coloquei a str do que eu quero colocar no MYSQL
 
-    print(f"{listar_manifestações}")  # inporta do banco e retorna num print
+    if manifestações == 0:
+        print("Nenhuma manifestação cadastrada")
+    else:
+         for n in range(len(manifestações)):
+            print(f"{n+1} - {manifestações[n]}")
+
+
+
+
+'''Tratamentos Necessários: 
+• Ao listar, caso não tenha nenhuma manifestação, informar ao usuário que não 
+existem manifestações cadastradas 
+
+ print(f"{listar_manifestações}")  # inporta do banco e retorna num print'''
