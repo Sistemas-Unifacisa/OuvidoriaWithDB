29bd21c9f1d6f45ee555bfa1d1cab731ef3c9bea
lucas.sarruda <lucassol133@gmail.com>
2025-04-01 23:43:11 -0300

    mudança do nome da def listar

--- def_listar.py
+++ def_listar.py
@@ -1,12 +1,12 @@
 from operacoesbd import listarBancoDados
 
 
-def listar_filmes(conn):
+def listar_manifestações(conn):
     sql = (
-        "SELECT * FROM filmes"  # definir em uma variavel o que eu quero passar pro SQL
+        "SELECT * FROM manifestacao"  # definir em uma variavel o que eu quero passar pro SQL
     )
 
-    lista_de_Filmes = listarBancoDados(conn, sql)
+    listar_manifestações = listarBancoDados(conn, sql)
     # com os paranmentros definidos, enviamos pro BANCO DE DADOS e ele vai ler o (sql) que eu coloquei a str do que eu quero colocar no MYSQL
 
-    print(f"{lista_de_Filmes}")  # inporta do banco e retorna num print
+    print(f"{listar_manifestações}")  # inporta do banco e retorna num print
