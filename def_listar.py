6762740e117aa67e85421ea9c3f4676902069693
Carlos L. ✔ <97265556+LopesLs@users.noreply.github.com>
2025-04-04 22:04:21 -0300

    ajustes para listar manifestações

--- def_listar.py
+++ def_listar.py
@@ -1,22 +1,24 @@
 from operacoesbd import listarBancoDados, encerrarConexao
 from connection import get_connection
 
-def listar_manifestacoes (conexão):
+
+def listar_manifestacoes():
     listar_Elogios()
     listar_Sugestoes()
     listar_Reclamacoes()
 
-def listar_manifestacoes_por_tipo(conexao):
+
+def listar_manifestacoes_por_tipo():
     menu = "1. Elogios\n2. Reclamações\n3. Sugestões\n4. Voltar\nEscolha uma opção:"
     while True:
-        try: 
+        try:
             opcao = int(input(menu))
             if opcao == 1:
-                listar_Elogios(conexao)
+                listar_Elogios()
             elif opcao == 2:
-                listar_Reclamacoes(conexao)
+                listar_Reclamacoes()
             elif opcao == 3:
-                listar_Sugestoes(conexao)
+                listar_Sugestoes()
             elif opcao == 4:
                 print("Voltando ao menu principal...")
                 break
@@ -26,8 +28,9 @@ def listar_manifestacoes_por_tipo(conexao):
             print("Entrada inválida. Por favor, insira um número.")
 
 
-def listar_Elogios(conexao):
-    get_connection()
+def listar_Elogios():
+    conexao = get_connection()
+
     # Consulta SQL para selecionar todas as manifestações do tipo 'Elogio'
     sql = "SELECT * FROM Elogio"
 
@@ -41,11 +44,14 @@ def listar_Elogios(conexao):
     # Imprime as manifestações do tipo 'Elogio' encontradas
     print("Elogios encontrados:")
     for elogio in elogios:
-        print(f"Código: {elogios[0]}, Descrição: {elogios[1]}")
+        print(f"Código: {elogio[0]}, Descrição: {elogio[1]}")
+
+    conexao.close()
 
 
-def listar_Reclamacoes(conexao):
-    get_connection()
+def listar_Reclamacoes():
+    conexao = get_connection()
+
     # Consulta SQL para selecionar todas as manifestações do tipo 'Reclamacao'
     sql = "SELECT * FROM Reclamacao"
 
@@ -59,11 +65,13 @@ def listar_Reclamacoes(conexao):
     # Imprime as manifestações do tipo 'Reclamacao' encontradas
     print("Reclamações encontradas:")
     for reclamacao in reclamacoes:
-        print(f"Código: {reclamacoes[0]}, Descrição: {reclamacoes[1]}")
+        print(f"Código: {reclamacao[0]}, Descrição: {reclamacao[1]}")
+
+    conexao.close()
 
 
-def listar_Sugestoes(conexao):
-    get_connection()
+def listar_Sugestoes():
+    conexao = get_connection()
     # Consulta SQL para selecionar todas as manifestações do tipo 'Sugestao'
     sql = "SELECT * FROM Sugestao"
 
@@ -77,4 +85,5 @@ def listar_Sugestoes(conexao):
     # Imprime as manifestações do tipo 'Sugestao' encontradas
     print("Sugestões encontradas:")
     for sugestao in sugestoes:
-        print(f"Código: {sugestoes[0]}, Descrição: {sugestoes[1]}")
+        print(f"Código: {sugestao[0]}, Descrição: {sugestao[1]}")
+    conexao.close()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Georgio Filho - Adicionar Filmes (feito )
 Ryan Matheus - Remover Filmes (Não sou vagabundo - Feito)
 """
 
-from def_listar import listar_manifestacoes_por_tipo
+from def_listar import listar_manifestacoes_por_tipo, listar_manifestacoes
 from operacoesbd import criarConexao, encerrarConexao
 from def_adicionar import adicionar_manifestacao
 from mostrar_menu import mostrar_menu
@@ -21,9 +21,9 @@ while True:
     opcao = int(input("> Escolha uma opção: "))
 
     if opcao == 1:  # Listagem das Manifestações
-        ...
+        listar_manifestacoes()
     elif opcao == 2:  # Listagem de Manifestações por Tipo
-        listar_manifestacoes_por_tipo(criarConexao())
+        listar_manifestacoes_por_tipo()
         ...
     elif opcao == 3:  # Criar uma nova Manifestação
         adicionar_manifestacao()
